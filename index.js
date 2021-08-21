98b5e5656e28dce4588a3fa16099293fd8fe208a
victor castro <victor_castro_01@hotmail.com>
2021-08-21 10:24:46 -0400

    Work in progress

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(superWin){
+   let wins;
+   for(let items of superWin){
+       if(superWin[items] === 'W'){
+            if(true){
+                return superWin.year = wins
+            }        
+       }
+    }   
+   return wins
+}     
+
+
 
